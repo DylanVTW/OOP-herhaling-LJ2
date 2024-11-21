7fe1f2f0dbf9c7c7f1d4f33c2bcaa5331aa5d1d3
Tielun <dylanvtw19@gmail.com>
2024-11-21 11:58:20 +0100

    bijles overzetting

---
+++ Bijles/character.php
@@ -0,0 +1,17 @@
+<?php
+
+class Character{
+    public $name;
+    public $health;
+    public $power;
+}
+function __construct($name, $health, $power){
+    $this->name = $name;
+    $this->health = $health;
+    $this->power = $power;
+}
+
+function attack(){
+
+}
+
